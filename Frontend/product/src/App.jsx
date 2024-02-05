eb3252cd0a2f972288fad7c1ea61ea2fedf52982
zahraaAed <zahraaalaaeddine94@gmail.com>
2024-02-05 16:51:40 +0200

    adding product

--- Frontend/product/src/App.jsx
+++ Frontend/product/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './Pages/login';
 import SignUp from './Pages/Registration';
+import Product from './Pages/product';
 function App() {
  
 
@@ -12,6 +13,7 @@ function App() {
     <Routes>
     <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
+        <Route path='/product' element={<Product />} />
     </Routes>
   </Router>
     </>
--- Frontend/product/src/Pages/product.jsx
+++ Frontend/product/src/Pages/product.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Modal from 'react-modal';
 
-function product() {
+const Product = () => {
+  const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    
+    getAllProducts();
+  }, []);
+
+  const getAllProducts = async () => {
+    try {
+      const response = await axios.get('http://localhost:4000/api/product');
+      setProducts(response.data.products);
+    } catch (error) {
+      console.error('Error fetching products:', error.message);
+    }
+  };
+
+  const getProductById = async (id) => {
+    try {
+      const response = await axios.get(`http://localhost:4000/api/product/${id}`);
+      setSelectedProduct(response.data);
+      setModalIsOpen(true);
+    } catch (error) {
+      console.error('Error fetching product by id:', error.message);
+    }
+  };
+
   return (
     <div>
-      
+      <h2>Product Page</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product._id}>
+            {product.title} -{' '}
+         
+            {product.image} -{' '}
+            <button onClick={() => getProductById(product._id)}>View Details</button>
+          </li>
+        ))}
+      </ul>
+
+      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+        {selectedProduct && (
+          <>
+            <h2>{selectedProduct.name}</h2>
+            <p>Description: {selectedProduct.description}</p>
+            <p>Price: {selectedProduct.price}</p>
+            <button onClick={() => setModalIsOpen(false)}>Close</button>
+          </>
+        )}
+      </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default product
-
+export default Product;
